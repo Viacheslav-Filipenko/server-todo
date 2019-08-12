e6214d3667eb80ef4fbecbbe4ed9774ab278ffc4
Viacheslav Filipenko <filipenko.slavik@gmail.com>
2019-08-13 00:22:09 +0300

    Add services

---
+++ services/todo.service.js
@@ -0,0 +1,19 @@
+exports.getAll = (req, res) => {
+
+}
+
+exports.getById = (req, res) => {
+
+}
+
+exports.create = (req, res) => {
+
+}
+
+exports.update = (req, res) => {
+
+}
+
+exports.delete = (req, res) => {
+
+}
